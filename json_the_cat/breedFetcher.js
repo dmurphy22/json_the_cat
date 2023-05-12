5c20d1dbeabccc92d2d7b953e235c9921fa2b4d2
Darragh Murphy <dmurphy@servuo.com>
2023-05-11 20:02:31 -0700

    Break out logic and add testing.

--- json_the_cat/breedFetcher.js
+++ json_the_cat/breedFetcher.js
@@ -1,15 +1,28 @@
 const request = require("request");
-const args = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
-  if (!error) {
-    const data = JSON.parse(body);
-    if (data.length < 1)
-      console.log("No results found");
-    console.log(data[0].description);
-    // console.log(data);
-  } else {
-    console.log("Something went wrong:", error.code);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
 
+    try {
+      const data = JSON.parse(body);
+
+      if (data.length < 1) {
+        callback("No results found", null);
+        return;
+      }
+
+      callback(null, data[0].description);
+    } catch (parseError) {
+      callback(parseError, null);
+    }
+  });
+};
+
+
+
+
+module.exports = { fetchBreedDescription };
